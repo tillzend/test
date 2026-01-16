49db65aaf8ed771cf6bc9e788fd3d6698948a161
Vova2 <volodyasyseov@gmail.com>
2026-01-16 12:13:16 +0200

    fresh 16.01.26

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 
-num_tickets = int(input("Введите число: "))
+num_tickets = int(input("enter value: "))
 bus_capacity = 48 
 
-full_bus_quantity = num_tickets // bus_capacity
+bus_quantity = num_tickets // bus_capacity
 num_tickets_left = num_tickets % bus_capacity
 
-print(full_bus_quantity, num_tickets_left)
+empty_seats = 0
+no_full_bus = False
+
+if num_tickets_left >= bus_capacity / 2 :
+    bus_quantity += 1
+    no_full_bus = True
+    empty_seats = num_tickets_left - bus_capacity
+
+
+print("count of full bus + 1: ", bus_quantity)
+print("left tickets: ", num_tickets_left)
+print("Incomplete bus: ", no_full_bus)
+print("Count of empty seats: ", empty_seats)

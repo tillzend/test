8e6038a051039ce84b7ea03f5579d1829788bf5d
Vova1 <volodyasysoev@gmail>
2026-01-20 19:35:33 +0200

    v.2 calculator(upgrade)

--- calculator.py
+++ calculator.py
@@ -3,40 +3,42 @@
 
 print("K.A.L.K.U.L.A.T.O.R")
 
-user_value_first = float(input("enter first value: "))
+user_value_first = float(input("enter first value: "))        # request values
 user_value_second = float(input("enter second value: "))
 
-answer = float
 
-if user_value_first == 0 and user_value_second == 0 :
-    print("ERROR: value cannot be alphabetic!")
-
-user_option = input("choose option: (+), (-), (*), (/), (//), (%) ")
-
-if user_option == "+":
-    answer = user_value_first + user_value_second 
-
-elif user_option == "-":
-    answer = user_value_first - user_value_second
-
-elif user_option == "*":
-    answer = user_value_first * user_value_second
-
-elif user_option == "/":
-    answer = user_value_first / user_value_second
-
-elif user_option == "//":
-    answer = user_value_first // user_value_second
-
-elif user_option == "%":
-    answer = user_value_first % user_value_second
-
-round_option = input("Do you want to round the value? (yes/no) ")
-
+answer = 0 
 round_number = 0
+round_option = str
 
-if round_option == "yes":
-    round_number = int(input("Round to how many decimal places? "))
-    print("Result: ", round(answer, round_number))
+
+if user_value_first == 0 or user_value_second == 0 : # check if any value equal zero
+    print("ERROR: value cannot be zero!")
 else:
-    print("Result: ", answer)
+    user_option = input("choose option: (+), (-), (*), (/), (//), (%) ")
+    round_option = input("Do you want to round the value? (yes/no) ")
+
+    if user_option == "+":
+        answer = user_value_first + user_value_second 
+
+    elif user_option == "-":
+        answer = user_value_first - user_value_second
+
+    elif user_option == "*":
+        answer = user_value_first * user_value_second
+
+    elif user_option == "/":
+        answer = user_value_first / user_value_second
+
+    elif user_option == "//":
+        answer = user_value_first // user_value_second
+
+    elif user_option == "%":
+       answer = user_value_first % user_value_second
+
+    if round_option == "yes" or round_option == "y":
+        round_number = int(input("Round to how many decimal places? "))
+        print("Result: ", round(answer, round_number))
+    else:
+
+        print("Result: ", answer)

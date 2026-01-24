5e91c81bc18baa3537f5d6216de81417bee9196f
Vova1 <volodyasysoev@gmail>
2026-01-24 10:06:21 +0200

    refe

--- reference.py
+++ reference.py
@@ -1,7 +1,46 @@
 
 import sys
 
-y = "5555"
-x = "5555"
+# y = "5555"
+# x = "5555"
 
-print(sys.getrefcount(y))
+# print(sys.getrefcount(y))
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+
+# print("is result:", a is b)
+
+# print("== result: ", a == b )
+
+# z = 50_000_000_000
+
+# c = 50_000_000_000.
+
+# y = .25
+
+# q = 24.
+
+# x = 0b01000001
+
+# print(c)
+
+
+#УРОК 49
+########################
+
+# user_answer = "ПрИВеТ!" 
+
+# word = "привет"
+
+# if word in user_answer.lower():
+#     print("привет")
+# else :
+#     print("false")
+
+user = "http://www.google.com"
+
+user_new = user.replace("http", "https")
+
+print(user_new)
+

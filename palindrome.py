75e68e5b468a0f82c3daedd0b4736058e1859c89
Vova1 <volodyasysoev@gmail>
2026-02-08 11:17:54 +0200

    upd

--- palindrome.py
+++ palindrome.py
@@ -9,13 +9,7 @@ if len(word) == "" or len(word) < 3:
     print("empty string or too short word! ")
 else:
     if pal_size % 2 == 0:
-        pal_size_t = pal_size - 1
-
+        pal_size_t = pal_size / 2
+        pal_size_partside = pal_size_t -1
     else:
-        None
-
-    
-
-
-
-
+        

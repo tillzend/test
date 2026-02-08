8553652333d4fa724cd8b427fe95c296b4ad721a
Vova1 <volodyasysoev@gmail>
2026-02-08 11:13:11 +0200

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,21 @@
+word = input("enter word : \n")
+
+is_palindrome = bool
+
+pal_size = len(word)
+pal_size_t = int
+
+if len(word) == "" or len(word) < 3:
+    print("empty string or too short word! ")
+else:
+    if pal_size % 2 == 0:
+        pal_size_t = pal_size - 1
+
+    else:
+        None
+
+    
+
+
+
+

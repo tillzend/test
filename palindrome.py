77297d417b9a7872831e3533ace64d9a426a020b
Vova1 <volodyasysoev@gmail>
2026-02-08 11:33:28 +0200

    first version pal

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,4 @@
-word = input("enter word : \n")
+word = "level"
 
 is_palindrome = bool
 
@@ -6,6 +6,9 @@ is_palindrome = bool
 if len(word) == "" or len(word) < 3:
     print("empty string or too short word! ")
 else:
-    
+    if word[0] == word[4] and word[1] == word[3]:
+        is_palindrome = True
+    else:
+        is_palindrome = False
 
-# print(is_palindrome)
+print(is_palindrome)

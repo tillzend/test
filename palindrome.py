2b68cb02b6125e4c3df2b1a717ec63b9c94de7b7
Vova1 <volodyasysoev@gmail>
2026-02-08 11:29:24 +0200

    upd

--- palindrome.py
+++ palindrome.py
@@ -2,14 +2,10 @@ word = input("enter word : \n")
 
 is_palindrome = bool
 
-pal_size = len(word)
-pal_size_t = int
 
 if len(word) == "" or len(word) < 3:
     print("empty string or too short word! ")
 else:
-    if pal_size % 2 == 0:
-        pal_size_t = pal_size / 2
-        pal_size_partside = pal_size_t -1
-    else:
-        
+    
+
+# print(is_palindrome)

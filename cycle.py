a50e983ab7a6d5176e1dbd42551a311e59234cfb
Vova1 <volodyasysoev@gmail>
2026-02-11 21:54:41 +0200

    upd circle

--- cycle.py
+++ cycle.py
@@ -32,3 +32,46 @@
 #         print(i)
 # else:
 #     print("well done" )
+
+
+# i = 0
+# circle = 0
+# total = 0
+
+# while i < 100:
+#     circle +=1
+#     i += 1
+#     total += i
+#     print (total ,"circle: " ,circle )
+# else:
+#     print("gjb")
+
+# n = int(input("enter value: \n"))
+
+# i = 0
+# result = 0
+
+# while i < 10:
+#     i += 1
+#     result = n * i
+
+#     print(f"{n} * {i} = {result}")
+# else:
+#     print("good")
+
+# password = input("Введите пароль: ")
+# word = "python"
+
+# while word != password :
+
+#     print("wrong password , try again!: ")
+#     password = input("Введите пароль: ")
+
+# num = int(input("Введите число: "))
+# count = 0
+
+# while num > 0:
+#     num = num // 10
+#     count += 1
+# else:
+#     print("good job: ", count)

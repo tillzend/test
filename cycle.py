ae54e099372dd6bfdc75f6729c723184ad939c47
Vova1 <volodyasysoev@gmail>
2026-02-11 21:27:55 +0200

    upd

---
+++ cycle.py
@@ -0,0 +1,34 @@
+
+# test "while" task
+
+# i = 1
+
+# while i < 10 :
+#     i += 1
+#     print(i)
+#     print("over and over again")
+    
+# else:
+#      print("good job()()")
+
+
+# n = int(input("enter value: \n"))
+
+# i = 1
+
+# while i < n :
+#     i += 1
+#     print(f"process: {i} of {n}")
+# else:
+#     print("well done" )
+
+# n = int(input("enter value: \n"))
+
+# i = 1
+
+# while i < n :
+#     i += 1
+#     if i % 2 == 0:
+#         print(i)
+# else:
+#     print("well done" )
